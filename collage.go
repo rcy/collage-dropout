e72d3c93b2634ac0ace922300000efaf45cc3523
Ryan Yeske <ryan@ryanyeske.com>
2023-04-23 15:09:37 -0700

    Working

--- collage.go
+++ collage.go
@@ -3,82 +3,52 @@ package main
 import (
 	"fmt"
 	"image"
-	"image/color"
-	"image/png"
 	"io/ioutil"
-	"math"
 	"math/rand"
-	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/disintegration/imaging"
-)
-
-const (
-	canvasWidth  = 800
-	canvasHeight = 800
-	maxImageSize = 200
-	maxRotate    = 20
+	"github.com/fogleman/gg"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	dir := "images"
+	output := "collage.png"
+	width, height := 1600, 1200
 
-	imagesDir := "./images"
-	images := []string{}
-
-	files, err := ioutil.ReadDir(imagesDir)
+	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		panic(err)
+		fmt.Println("Error reading directory:", err)
+		return
 	}
+
+	rand.Seed(time.Now().UnixNano())
+	dc := gg.NewContext(width, height)
 
 	for _, file := range files {
-		if filepath.Ext(file.Name()) == ".jpg" ||
-			filepath.Ext(file.Name()) == ".jpeg" ||
-			filepath.Ext(file.Name()) == ".png" ||
-			filepath.Ext(file.Name()) == ".gif" {
-			images = append(images, filepath.Join(imagesDir, file.Name()))
+		if !file.IsDir() {
+			imgPath := filepath.Join(dir, file.Name())
+			img, err := gg.LoadImage(imgPath)
+			if err != nil {
+				fmt.Println("Error loading image:", err)
+				continue
+			}
+
+			rotateAngle := rand.Float64()*20 - 10
+			rotatedImg := imaging.Rotate(img, rotateAngle, image.Transparent)
+
+			x := rand.Float64() * float64(width-rotatedImg.Bounds().Dx())
+			y := rand.Float64() * float64(height-rotatedImg.Bounds().Dy())
+
+			dc.DrawImage(rotatedImg, int(x), int(y))
 		}
 	}
 
-	createCollage(images)
-}
-
-func createCollage(images []string) {
-	canvas := imaging.New(canvasWidth, canvasHeight, color.White)
-
-	for _, imagePath := range images {
-		img, err := imaging.Open(imagePath)
-		if err != nil {
-			continue
-		}
-
-		aspectRatio := float64(img.Bounds().Size().X) / float64(img.Bounds().Size().Y)
-		imageWidth := math.Min(float64(maxImageSize), float64(img.Bounds().Size().X))
-		imageHeight := imageWidth / aspectRatio
-
-		x := rand.Intn(canvasWidth - int(imageWidth))
-		y := rand.Intn(canvasHeight - int(imageHeight))
-		rotate := rand.Float64()*float64(maxRotate*2) - float64(maxRotate)
-
-		img = imaging.Resize(img, int(imageWidth), int(imageHeight), imaging.Lanczos)
-		img = imaging.Rotate(img, rotate, color.Transparent)
-
-		canvas = imaging.Paste(canvas, img, image.Pt(x, y))
-	}
-
-	file, err := os.Create("collage.png")
+	err = dc.SavePNG(output)
 	if err != nil {
-		panic(err)
+		fmt.Println("Error saving collage image:", err)
+		return
 	}
-
-	defer file.Close()
-
-	err = png.Encode(file, canvas)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Collage saved as collage.png")
+	fmt.Println("Collage image saved as", output)
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.17
 
 require (
 	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 )
